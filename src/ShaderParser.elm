0e1e6b8b2311b109462623e2f60ec1a6ee8fe7e8
xarvh <francesco.orsenigo@gmail.com>
2018-08-17 13:57:45 +0200

    Have parser skeleton

--- src/ShaderParser.elm
+++ src/ShaderParser.elm
@@ -6,143 +6,272 @@ import Set
 
 
 {-
+      [identity a = a]
 
 
-   [addition a b {number number number}
-     (+ a b)
-   ]
+      [addition a b {number number number}
+        (+ a b)
+      ]
 
 
-   [someFunction b {number number}
-     [s 0]
-     [e 20]
+      [someFunction b {number number}
+        [s 0]
+        [e 20]
 
-     (- e s)
-   ]
-
-
-
-    (if someCondition
-      (hugeIfTrue)
-
-      (
-        [ x {number} 3 ]
-        [ y -2 ]
-        dotProduct x y
-      )
-
-    )
+        (- e s)
+      ]
 
 
 
-parseAssignment
-  "["
-  oneOrMore parseVariable
-  maybe parseType
-  parseValue
-  "]"
+       (if someCondition
+         (hugeIfTrue)
+
+         (
+           [ x {number} 3 ]
+           [ y -2 ]
+           dotProduct x y
+         )
+
+       )
 
 
-parseType
-  "{"
-  oneOrMore parseVariable
-  "}"
+
+   parseAssignment
+     "["
+     oneOrMore parseVariable
+     maybe parseType
+     parseValue
+     "]"
 
 
-parseValue
-  oneOf
-    parseConstant
-    parseVariable
-    parseParens
+   parseType
+     "{"
+     oneOrMore parseVariable
+     "}"
 
 
-parseParens
-  "("
-  oneOrMore parseAssignment
-  oneOrMore
-    parseValue
-  ")"
+   parseValue
+     oneOf
+       parseConstant
+       parseVariable
+       parseParens
+
+
+   parseParens
+     "("
+     oneOrMore parseAssignment
+     oneOrMore parseValue
+     ")"
 
 -}
-
-
-map2 : (a -> b -> c) -> Parser ( a, b ) -> Parser c
-map2 f =
-    Parser.map (\( a, b ) -> f a b)
-
-
-
----
 
 
 type alias VariableName =
     String
 
 
-type Expression
-    = AttributeAccess Expression AttributeName
-    | AttributeGetter AttributeName
-    | Variable VariableName
-    | ConstantInteger Int
+type Value
+    = ValueConstant Constant
+    | ValueVariable VariableName
+    | ValueExpression (List Assignment) (List Value)
+
+
+type Constant
+    = ConstantString String
+    | ConstantInt Int
     | ConstantFloat Float
-    | Parens Expression
-    | UnaryOperation Expression
-    | FunctionCall Expression (List Expression)
-    | BinaryOperation Expression Expression
-    | LetIn (List Assignment) Expression
 
 
-expression : Parser Expression
-expression =
-    [ Parser.lazy (\_ -> map2 AttributeAccess attributeAccess)
-    , Parser.map AttributeGetter attributeGetter
-    , Parser.map Variable variable
-    , Parser.map ConstantInteger Parser.int
-    , Parser.map ConstantFloat Parser.float
-    , Parser.lazy (\_ -> map Parens parensExpression)
-    ]
-        -- TODO This is the anti-patternest of them all, but will stay here
-        -- until Parser gets better docs.
-        |> List.map Parser.backtrackable
-        |> Parser.oneOf
+type alias Assignment =
+    { name : String
+    , args : List String
+    , maybeTypeAnnotation : Maybe (List String)
+    , value : Value
+    }
 
 
-variable : Parser String
-variable =
+
+--
+
+
+parse =
+    assignment
+
+
+
+--
+
+
+variableName : Parser String
+variableName =
     Parser.variable
         { start = Char.isAlpha
-        , inner = \c -> Char.isAlphaNum c || c == '_'
-        , reserved = Set.fromList reservedNames
+        , inner = \c -> Char.isAlphaNum c || c == '_' || c == '/'
+        , reserved = Set.empty
         }
 
 
-parensExpression : Parser Expression
-parensExpression =
+assignment : Parser Assignment
+assignment =
+    Parser.succeed Assignment
+        |. Parser.symbol "["
+        |. Parser.spaces
+        |= variableName
+        |. Parser.spaces
+        |= zeroOrMore variableName
+        |. Parser.spaces
+        |= maybe typeAnnotation
+        |. Parser.spaces
+        |. Parser.symbol "="
+        |. Parser.spaces
+        |= value
+        |. Parser.spaces
+        |. Parser.symbol "]"
+
+
+typeAnnotation : Parser (List String)
+typeAnnotation =
     Parser.succeed identity
+        |. Parser.symbol "{"
+        |. Parser.spaces
+        |= oneOrMore variableName
+        |. Parser.spaces
+        |. Parser.symbol "}"
+
+
+value : Parser Value
+value =
+    Parser.oneOf
+        [ Parser.map ValueVariable variableName
+        , Parser.lazy (\_ -> parensExpression)
+        ]
+
+
+parensExpression : Parser Value
+parensExpression =
+    Parser.succeed ValueExpression
         |. Parser.symbol "("
         |. Parser.spaces
-        |= expression
+        |= zeroOrMore assignment
+        |. Parser.spaces
+        |= oneOrMore value
         |. Parser.spaces
         |. Parser.symbol ")"
 
 
-attributeAccess : Parser ( Expression, AttributeName )
-attributeAccess =
-    Parser.succeed Tuple.pair
-        |= Parser.oneOf [ parensExpression, map Variable variable ]
-        |. Parser.symbol "."
-        |= variable
+
+-----------------
 
 
-attributeGetter : Parser AttributeName
-attributeGetter =
-    Parser.succeed identity
-        |. Parser.symbol "."
-        |= variable
+maybe : Parser a -> Parser (Maybe a)
+maybe parser =
+    Parser.oneOf
+        [ Parser.map Just parser
+        , Parser.succeed Nothing
+        ]
 
 
-parse : Parser Expression
-parse =
-    Parser.succeed identity
-        |= expression
-        |. Parser.end
+zeroOrMore : Parser a -> Parser (List a)
+zeroOrMore parser =
+    listHelp parser []
+
+
+oneOrMore : Parser a -> Parser (List a)
+oneOrMore parser =
+    parser |> Parser.andThen (\n -> listHelp parser [ n ])
+
+
+listHelp : Parser a -> List a -> Parser (List a)
+listHelp parser listSoFar =
+    Parser.oneOf
+        [ next parser |> Parser.andThen (\a -> listHelp parser (a :: listSoFar))
+        , Parser.succeed (List.reverse listSoFar)
+        ]
+
+
+next : Parser a -> Parser a
+next parser =
+    Parser.backtrackable <|
+        Parser.succeed identity
+            |. Parser.spaces
+            |= parser
+
+
+
+{-
+
+
+   --------
+
+   map2 : (a -> b -> c) -> Parser ( a, b ) -> Parser c
+   map2 f =
+       Parser.map (\( a, b ) -> f a b)
+
+
+
+   ---
+
+
+
+
+   type Expression
+       = AttributeAccess Expression AttributeName
+       | AttributeGetter AttributeName
+       | Variable VariableName
+       | ConstantInteger Int
+       | ConstantFloat Float
+       | Parens Expression
+       | UnaryOperation Expression
+       | FunctionCall Expression (List Expression)
+       | BinaryOperation Expression Expression
+       | LetIn (List Assignment) Expression
+
+
+   expression : Parser Expression
+   expression =
+       [ Parser.lazy (\_ -> map2 AttributeAccess attributeAccess)
+       , Parser.map AttributeGetter attributeGetter
+       , Parser.map Variable variable
+       , Parser.map ConstantInteger Parser.int
+       , Parser.map ConstantFloat Parser.float
+       , Parser.lazy (\_ -> map Parens parensExpression)
+       ]
+           -- TODO This is the anti-patternest of them all, but will stay here
+           -- until Parser gets better docs.
+           |> List.map Parser.backtrackable
+           |> Parser.oneOf
+
+
+
+
+   parensExpression : Parser Expression
+   parensExpression =
+       Parser.succeed identity
+           |. Parser.symbol "("
+           |. Parser.spaces
+           |= expression
+           |. Parser.spaces
+           |. Parser.symbol ")"
+
+
+   attributeAccess : Parser ( Expression, AttributeName )
+   attributeAccess =
+       Parser.succeed Tuple.pair
+           |= Parser.oneOf [ parensExpression, map Variable variable ]
+           |. Parser.symbol "."
+           |= variable
+
+
+   attributeGetter : Parser AttributeName
+   attributeGetter =
+       Parser.succeed identity
+           |. Parser.symbol "."
+           |= variable
+
+
+   parse : Parser Expression
+   parse =
+       Parser.succeed identity
+           |= expression
+           |. Parser.end
+-}
